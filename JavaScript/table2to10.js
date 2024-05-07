7d9b14ea4e9ec724326dea3497ecb9301ea863f3
Aniket Bante <150164344+aniket7334@users.noreply.github.com>
2024-05-07 18:31:16 +0530

    Update table2to10.js

--- JavaScript/table2to10.js
+++ JavaScript/table2to10.js
@@ -3,5 +3,5 @@ for (let i = 2; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
         console.log(`${i} * ${j} = ${i * j}`);
     }
-    console.log(); // Empty line for separation between tables
+    console.log(); 
 }
