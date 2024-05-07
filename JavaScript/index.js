5fdd9ea4d91e10c7449aceacfa94ffd3f9846519
Aniket Bante <150164344+aniket7334@users.noreply.github.com>
2024-05-07 18:45:00 +0530

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,7 +5,10 @@ console .log(" Print the numbers form 1 to 10")
   console.log(i)
  }
 
-// for(let a=1;a<=100;a = a + 2)
-// {
-//     console.log(a)
-// }
+console.log("")
+
+console.log("Print odd numbers between 1 to 100")
+ for(let a=1;a<=100;a = a + 2)
+ {
+     console.log(a)
+ }
