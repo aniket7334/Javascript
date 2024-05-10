e4009a4fb5f203821ad58ade2caefdb8ea0b2247
Aniket Bante <150164344+aniket7334@users.noreply.github.com>
2024-05-10 15:34:48 +0530

    Update min.js

--- JavaScript/min.js
+++ JavaScript/min.js
@@ -1,15 +1,6 @@
 const array =[34,65,85,45,35,21,52,20,15];
 
-let min = array[0];
-
-for(let i = 0; i < array.length; i++){
-
-    if( array[i] < min ){
-        min = array [i];
-    }
-
-    
-}
+let min = Math.min.apply(null, array);
 console.log(min);
 
 
