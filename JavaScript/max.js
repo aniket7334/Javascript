6b4f4ca93294c7beb9495517ba85d9aecf333ad1
Aniket Bante <150164344+aniket7334@users.noreply.github.com>
2024-05-10 15:32:59 +0530

    Update max.js

--- JavaScript/max.js
+++ JavaScript/max.js
@@ -1,15 +1,8 @@
 const points = [52,65,84,75,98,25,35,99];
+let max = Math.max.apply (null , points);
 
-
-
-let max = points[0];
-for(let i = 0; i < points.length;i++){
-
-    if(points[i] > max){
-       max = points[i];
-      }
-      
-  }
   console.log( "print maximum number in array " ,max);
+
+
   
 
